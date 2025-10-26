91e10e7c13a9802e3ee516c87c2959a57ab2342c
Your Name <you@example.com>
2025-10-26 03:06:28 +0100

    restored models to Docker file

--- Dockerfile
+++ Dockerfile
@@ -55,10 +55,18 @@ ARG MODEL_TYPE
 WORKDIR /comfyui
 
 # Download checkpoints/vae/LoRA to include in image based on model type
+RUN wget -P ./models/text_encoders https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors
+RUN wget -P ./models/text_encoders https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn_scaled.safetensors
+RUN wget -P ./models/vae https://huggingface.co/Comfy-Org/Lumina_Image_2.0_Repackaged/resolve/main/split_files/vae/ae.safetensors
+RUN wget -P ./models/diffusion_models https://huggingface.co/Comfy-Org/FLUX.1-Krea-dev_ComfyUI/resolve/main/split_files/diffusion_models/flux1-krea-dev_fp8_scaled.safetensors
+RUN wget -P ./models/diffusion_models https://huggingface.co/Comfy-Org/flux1-kontext-dev_ComfyUI/resolve/main/split_files/diffusion_models/flux1-dev-kontext_fp8_scaled.safetensors
+RUN wget -P ./models/upscale_models https://huggingface.co/sberbank-ai/Real-ESRGAN/resolve/main/RealESRGAN_x2.pth
 
 # Stage 3: Final image
 FROM base as final
 
+# Copy models from stage 2 to the final image
+COPY --from=downloader /comfyui/models /comfyui/models
 
 # Start the container
 CMD /start.sh
--- src/start.sh
+++ src/start.sh
@@ -3,12 +3,6 @@
 # Use libtcmalloc for better memory management
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
-[ ! -f /comfyui/models/text_encoders/clip_l.safetensors ] && wget -P /comfyui/models/text_encoders https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors
-[ ! -f /comfyui/models/text_encoders/t5xxl_fp8_e4m3fn_scaled.safetensors ] && wget -P /comfyui/models/text_encoders https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn_scaled.safetensors
-[ ! -f /comfyui/models/vae/ae.safetensors ] && wget -P /comfyui/models/vae https://huggingface.co/Comfy-Org/Lumina_Image_2.0_Repackaged/resolve/main/split_files/vae/ae.safetensors
-[ ! -f /comfyui/models/diffusion_models/flux1-krea-dev_fp8_scaled.safetensors ] && wget -P /comfyui/models/diffusion_models https://huggingface.co/Comfy-Org/FLUX.1-Krea-dev_ComfyUI/resolve/main/split_files/diffusion_models/flux1-krea-dev_fp8_scaled.safetensors
-[ ! -f /comfyui/models/diffusion_models/flux1-dev-kontext_fp8_scaled.safetensors ] && wget -P /comfyui/models/diffusion_models https://huggingface.co/Comfy-Org/flux1-kontext-dev_ComfyUI/resolve/main/split_files/diffusion_models/flux1-dev-kontext_fp8_scaled.safetensors
-[ ! -f /comfyui/models/upscale_models/RealESRGAN_x2.pth ] && wget -P /comfyui/models/upscale_models https://huggingface.co/sberbank-ai/Real-ESRGAN/resolve/main/RealESRGAN_x2.pth
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
